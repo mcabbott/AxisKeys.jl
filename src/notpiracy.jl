9e48a94dfebb4ffc87d46f6a4c6ebc78d5fcd4b3
Michael Abbott <me@escbook>
2020-03-25 12:46:07 +0100

    tidy up notpiracy.jl, methods of findindex

--- src/notpiracy.jl
+++ src/notpiracy.jl
@@ -1,104 +1,55 @@
-#===== Adding new functionality =====#
-
 #=
+All that's left here is some faster replacements for things like findall(<=(3), range).
+The ones in Base always return an Array, while these return ranges.
+A messy collection, ideally to be replaced by this:
+https://github.com/JuliaMath/IntervalSets.jl/issues/52#issuecomment-603767916
 
-# https://github.com/JuliaLang/julia/pull/32968
-filter(args...) = Base.filter(args...)
-filter(f, xs::Tuple) = Base.afoldl((ys, x) -> f(x) ? (ys..., x) : ys, (), xs...)
-filter(f, t::Base.Any16) = Tuple(filter(f, collect(t)))
-
-# https://github.com/JuliaLang/julia/pull/30496 -> Compat.jl
-if VERSION < v"1.2.0-DEV.272"
-    Base.@pure hasfield(::Type{T}, name::Symbol) where T =
-        Base.fieldindex(T, name, false) > 0
-    hasproperty(x, s::Symbol) = s in propertynames(x)
-end
-
-# Treat Ref() like a 1-tuple in map:
-map(args...) = Base.map(args...)
-map(f, r::Base.RefValue) = error("one arg") # Ref(f(r[]))
-map(f, r::Base.RefValue, t::Tuple) = error("ref first") # Ref(f(r[], first(t)))
-map(f, t::Tuple, r::Base.RefValue) = error("ref second") # Ref(f(first(t), r[]))
-
+Replacements for findfirst(==(3), range) and findall(==(3), range) moved to lookup.jl.
 =#
 
-# using Compat # 2.0 hasfield + 3.1 filter
-
-#===== Speeding up with same results =====#
-
-# findfirst(args...) = Base.findfirst(args...)
-findall(args...) = Base.findall(args...)
-
-#=
-for equal in (isequal, Base.:(==))
-    @eval begin
-
-# findfirst returns always Int or Nothing
-# BTW see https://github.com/JuliaLang/julia/pull/30778
-
-        findfirst(eq::Base.Fix2{typeof($equal),Int}, r::Base.OneTo{Int}) =
-            1 <= eq.x <= r.stop ? eq.x : nothing
-
-        findfirst(eq::Base.Fix2{typeof($equal)}, r::AbstractUnitRange) =
-            first(r) <= eq.x <= last(r) ? 1+Int(eq.x - first(r)) : nothing
-
-# findall returns a vector... which I would like to make a range?
-
-        findall(eq::Base.Fix2{typeof($equal),Int}, r::Base.OneTo{Int}) =
-            1 <= eq.x <= r.stop ? (eq.x:eq.x) : (1:0)
-
-        function findall(eq::Base.Fix2{typeof($equal)}, r::AbstractUnitRange)
-            val = 1 + Int(eq.x - first(r))
-            first(r) <= eq.x <= last(r) ? (val:val) : (1:0)
-        end
-
-    end
-end
-=#
-
-findall(eq::Base.Fix2{typeof(<=),Int}, r::Base.OneTo{Int}) =
+findindex(eq::Base.Fix2{typeof(<=),Int}, r::Base.OneTo{Int}) =
     eq.x < 1 ? Base.OneTo(0) :
     eq.x >= r.stop ? r :
     Base.OneTo(eq.x)
 
-findall(eq::Base.Fix2{typeof(<=)}, r::UnitRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(<=)}, r::UnitRange{T}) where T =
     intersect(Base.OneTo(trunc(T,eq.x) - first(r) + 1), eachindex(r))
 
-findall(eq::Base.Fix2{typeof(<=)}, r::StepRange{T}) where T =
-# findall(eq::Base.Fix2{typeof(<=)}, r::AbstractRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(<=)}, r::StepRange{T}) where T =
+# findindex(eq::Base.Fix2{typeof(<=)}, r::AbstractRange{T}) where T =
     eq.x < r.start ? OneTo(0) :
     eq.x >= r.stop ? OneTo(length(r)) :
     OneTo(trunc(Int, div(eq.x - first(r), step(r))) + 1)
 
-findall(eq::Base.Fix2{typeof(<),Int}, r::Base.OneTo{Int}) =
+findindex(eq::Base.Fix2{typeof(<),Int}, r::Base.OneTo{Int}) =
     eq.x <= 1 ? Base.OneTo(0) :
     eq.x > r.stop ? r :
     Base.OneTo(eq.x - 1)
 
-findall(eq::Base.Fix2{typeof(<)}, r::UnitRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(<)}, r::UnitRange{T}) where T =
     intersect(Base.OneTo(trunc(T,eq.x - first(r))), eachindex(r))
 
-# findall(eq::Base.Fix2{typeof(<)}, r::StepRange{T}) where T =
+# findindex(eq::Base.Fix2{typeof(<)}, r::StepRange{T}) where T =
 #     Base.OneTo(trunc(Int, div(eq.x - first(r) - 1, step(r))) + 1)
 
-findall(eq::Base.Fix2{typeof(>=),Int}, r::Base.OneTo{Int}) =
+findindex(eq::Base.Fix2{typeof(>=),Int}, r::Base.OneTo{Int}) =
     eq.x <= 1 ? UnitRange(r) :
     eq.x > r.stop ? (1:0) :
     (eq.x : r.stop)
 
-findall(eq::Base.Fix2{typeof(>=)}, r::UnitRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(>=)}, r::UnitRange{T}) where T =
     intersect(UnitRange(trunc(T,eq.x) - first(r) + 1, length(r)), eachindex(r))
 
-findall(eq::Base.Fix2{typeof(>),Int}, r::Base.OneTo{Int}) =
+findindex(eq::Base.Fix2{typeof(>),Int}, r::Base.OneTo{Int}) =
     eq.x < 1 ? UnitRange(r) :
     eq.x >= r.stop ? (1:0) :
     (eq.x+1 : r.stop)
 
-findall(eq::Base.Fix2{typeof(>)}, r::OrdinalRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(>)}, r::OrdinalRange{T}) where T =
     intersect(UnitRange(trunc(T,eq.x) - first(r) + 2, length(r)), eachindex(r))
 
-findall(eq::Base.Fix2{typeof(>)}, r::StepRange{T}) where T =
-# findall(eq::Base.Fix2{typeof(>)}, r::AbstractRange{T}) where T =
+findindex(eq::Base.Fix2{typeof(>)}, r::StepRange{T}) where T =
+# findindex(eq::Base.Fix2{typeof(>)}, r::AbstractRange{T}) where T =
     eq.x < r.start ? (1:length(r)) :
     eq.x >= r.stop ? (1:0) :
     UnitRange(trunc(Int, div(eq.x - first(r), step(r))) + 2, length(r))
--- test/_notpiracy.jl
+++ test/_notpiracy.jl
@@ -1,64 +1,43 @@
-#=
-@testset "filter" begin # now using Compat
+using Test, AxisKeys
 
-    @test AxisKeys.filter(iseven, (1,2,3,4)) === (2,4)
-    if VERSION >= v"1.2" # fails on 1.0
-        @test 0 == @allocated AxisKeys.filter(iseven, (1,2,3,4))
-    end
-
-end
-@testset "map" begin # not required anymore
-
-    @test AxisKeys.map(sqrt, Ref(4))[] == 2.0
-    @test AxisKeys.map(sqrt, Ref(4)) isa Ref
-
-    @test AxisKeys.map(+, Ref(2), (3,))[] == 5
-    @test AxisKeys.map(+, Ref(2), (3,)) isa Ref
-    @test AxisKeys.map(+, (2,), Ref(3)) isa Ref
-    @test 0 == @allocated AxisKeys.map(+, (2,), Ref(3))
-
-end
-=#
 if VERSION >= v"1.2" # <(3) doesn't exist on 1.1, but Base.Fix2 is fine
-@testset "unit ranges" begin
+@testset "unit ranges" begin # now methods of AxisKeys.findindex, not Base.findfirst etc.
 
     for r in (Base.OneTo(5), 2:5)
         for x in -2:7
 
-            @test AxisKeys.findfirst(==(x), r) == findfirst(==(x), collect(r))
-            @test AxisKeys.findfirst(isequal(x), r) == findfirst(isequal(x), collect(r))
+            @test AxisKeys.findindex(x, r) == findfirst(==(x), collect(r))
 
-            # for op in (isequal, Base.:(==), Base.:<, Base.:<=, Base.:>, Base.:>=)
-            for op in (Base.:<, Base.:<=, Base.:>, Base.:>=)
+            for op in (isequal, Base.:(==), Base.:<, Base.:<=, Base.:>, Base.:>=)
 
-                @test AxisKeys.findall(op(x), r) == Base.findall(op(x), collect(r))
-                @test AxisKeys.findall(op(x), r) isa AbstractRange
-                T = typeof(@inferred AxisKeys.findall(op(x), r))
-                T <: AbstractRange || @info "$op($x) $r  -> $T"
+                @test AxisKeys.findindex(op(x), r) == Base.findall(op(x), collect(r))
+                @test AxisKeys.findindex(op(x), r) isa AbstractRange
+                T = typeof(@inferred AxisKeys.findindex(op(x), r))
+                T <: AbstractRange || @info "findindex($op($x), $r) isa $T, unit ranges"
             end
         end
     end
 
 end
-@testset "step ranges" begin
+@testset "step ranges" begin # now methods of AxisKeys.findindex, not Base.finall
 
     for op in (Base.:<=, Base.:>, )# Base.:<)
 
         for r in (0:3:9, )# 0.5:0.5:8) # stepranges?
         for x in [-0.1, 0, 0.2, 0.5, 0.7, 1.0, 1.2,  1.9, 2.0, 2.1, 7.9, 8, 8.1, 7.9, 8, 8.1]
 
-            @test AxisKeys.findall(op(x), r) == Base.findall(op(x), collect(r))
-            T = typeof(AxisKeys.findall(op(x), r))
-            T <: AbstractRange || @info "$op($x) $r  -> $T"
+            @test AxisKeys.findindex(op(x), r) == Base.findall(op(x), collect(r))
+            T = typeof(AxisKeys.findindex(op(x), r))
+            T <: AbstractRange || @info "findindex($op($x), $r) isa $T, step ranges"
         end
         end
 
         for r in ('b':'e', 'b':2:'f')
         for x in 'a':'g'
 
-            @test AxisKeys.findall(op(x), r) == Base.findall(op(x), collect(r))
-            T = typeof(@inferred AxisKeys.findall(op(x), r))
-            T <: AbstractRange || @info "$op($x) $r  -> $T"
+            @test AxisKeys.findindex(op(x), r) == Base.findall(op(x), collect(r))
+            T = typeof(@inferred AxisKeys.findindex(op(x), r))
+            T <: AbstractRange || @info "findindex($op($x), $r) isa $T, step ranges of Char"
         end
         end
 
