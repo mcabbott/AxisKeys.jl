66b01f4823e3d0d1513046b08886ac010f491bf1
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-10-03 09:03:43 -0400

    formatting

--- test/_functions.jl
+++ test/_functions.jl
@@ -61,17 +61,18 @@ A3 = wrapdims(rand(Int8, 3,4,2), r='a':'c', c=2:5, p=[10.0, 20.0])
         @test reshape(M, (4,3)) isa Array
         @test reshape(M, (2,:)) isa Array
     end
+
 end
-@testset "Regression test against https://github.com/mcabbott/AxisKeys.jl/issues/43" begin
+@testset "mapreduce & keywords, issue 43" begin
+    # https://github.com/mcabbott/AxisKeys.jl/issues/43
+
     z = KeyedArray(zeros(3); foo=[:a, :b, :c])
     @test mapreduce(identity, +, z; init=10) == 10
 
     m = KeyedArray([1, 2, 3]; foo=[:a, :b, :c])
     @test std(m; corrected=false) == std([1, 2, 3]; corrected=false)
 
-
 end
-
 @testset "sort" begin
 
     @test sort(V)(20) == V(20)
