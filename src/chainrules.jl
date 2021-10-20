c36f4a3a44c5c2c339b7a47ad91b876d1f3600e5
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-10-20 21:48:28 +0100

    Simplify

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,16 +1,15 @@
 using ChainRulesCore
 
 _keyless_unname_pullback(ȳ) =  (NoTangent(), ȳ)
-_keyless_unname_pullback(ȳ::Tangent) = _KeyedArray_pullback(ȳ.data)
-_keyless_unname_pullback(ȳ::AbstractThunk) = _KeyedArray_pullback(unthunk(ȳ))
+_keyless_unname_pullback(ȳ::Tangent) = __keyless_unname_pullback(ȳ.data)
+_keyless_unname_pullback(ȳ::AbstractThunk) = _keyless_unname_pullback(unthunk(ȳ))
 
 function ChainRulesCore.rrule(::typeof(keyless_unname), x::KeyedArray)
     project_x = ProjectTo(x.data)
-    keyless_unname_pb(y) = _keyless_unname_pullback(project_x(y))
-    return keyless_unname(x), keyless_unname_pb
+    keyless_unname_pullback(y) = _keyless_unname_pullback(project_x(y))
+    return keyless_unname(x), keyless_unname_pullback
 end
 
 function ChainRulesCore.rrule(::typeof(keyless_unname), x)
-    keyless_unname_pb(y) = _keyless_unname_pullback(y)
-    return keyless_unname(x), keyless_unname_pb
+    return keyless_unname(x), _keyless_unname_pullback
 end
