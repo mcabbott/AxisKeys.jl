9bffa9352bf212b2d8a9451291e046a4181bd6b0
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-10-20 21:47:23 +0100

    Rename KeyedArray_pullback -> keyless_unname_pullback

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,16 +1,16 @@
 using ChainRulesCore
 
-_KeyedArray_pullback(ȳ) =  (NoTangent(), ȳ)
-_KeyedArray_pullback(ȳ::Tangent) = _KeyedArray_pullback(ȳ.data)
-_KeyedArray_pullback(ȳ::AbstractThunk) = _KeyedArray_pullback(unthunk(ȳ))
+_keyless_unname_pullback(ȳ) =  (NoTangent(), ȳ)
+_keyless_unname_pullback(ȳ::Tangent) = _KeyedArray_pullback(ȳ.data)
+_keyless_unname_pullback(ȳ::AbstractThunk) = _KeyedArray_pullback(unthunk(ȳ))
 
 function ChainRulesCore.rrule(::typeof(keyless_unname), x::KeyedArray)
     project_x = ProjectTo(x.data)
-    keyless_unname_pb(y) = _KeyedArray_pullback(project_x(y))
+    keyless_unname_pb(y) = _keyless_unname_pullback(project_x(y))
     return keyless_unname(x), keyless_unname_pb
 end
 
 function ChainRulesCore.rrule(::typeof(keyless_unname), x)
-    keyless_unname_pb(y) = _KeyedArray_pullback(y)
+    keyless_unname_pb(y) = _keyless_unname_pullback(y)
     return keyless_unname(x), keyless_unname_pb
 end
