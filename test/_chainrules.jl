78ef5af356d0437b615816144ce1da9ca6b71706
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-10-20 18:11:23 +0100

    Organise tests

--- test/_chainrules.jl
+++ test/_chainrules.jl
@@ -12,9 +12,23 @@
         return v, back
     end
 
-    test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), a=1:3); check_inferred=false)
-    test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3, 4), a=1:3, b='a':'d'); check_inferred=false)
-    test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), a=1:3); output_tangent=rand(3, 1), check_inferred=false)
-    test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), 1:3); check_inferred=false)
-    test_rrule(AxisKeys.keyless_unname, ones(3); check_inferred=false)
+    @testset "KeyedVector" begin
+        data = rand(3)
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, a=1:3); check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, 1:3); check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, data; check_inferred=false)
+
+        # with matrix output tangent
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, a=1:3); output_tangent=rand(3, 1), check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, 1:3); output_tangent=rand(3, 1), check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, data; output_tangent=rand(3, 1), check_inferred=false)
+    end
+
+    @testset "KeyedMatrix" begin
+        data = rand(3, 4)
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, a=1:3, b='a':'d'); check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, wrapdims(data, 1:3, 'a':'d'); check_inferred=false)
+        test_rrule(AxisKeys.keyless_unname, data; check_inferred=false)
+    end
+
 end
