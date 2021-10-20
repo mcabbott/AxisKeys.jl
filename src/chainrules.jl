ca8a89d5382612c8ee8f99d4373c3548b345ddcd
Glenn Moynihan <glenn.moynihan@invenialabs.co.uk>
2021-10-20 17:48:08 +0100

    Fix rrule for variables without names

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,11 +1,22 @@
 using ChainRulesCore
 
-_KeyedArray_pullback(ȳ::AbstractArray, keys) =  (NoTangent(), wrapdims(ȳ; keys...))
+_KeyedArray_pullback(ȳ::AbstractArray, keys) =  (NoTangent(), wrapdims(ȳ, keys...))
 _KeyedArray_pullback(ȳ::Tangent, keys) = _KeyedArray_pullback(ȳ.data, keys)
 _KeyedArray_pullback(ȳ::AbstractThunk, keys) = _KeyedArray_pullback(unthunk(ȳ), keys)
 
-function ChainRulesCore.rrule(::typeof(keyless_unname), x)
+function ChainRulesCore.rrule(::typeof(keyless_unname), x::KaNda)
     project_x = ProjectTo(x.data)
     pb(y) = _KeyedArray_pullback(project_x(y), named_axiskeys(x))
+    return keyless_unname(x), pb
+end
+
+function ChainRulesCore.rrule(::typeof(keyless_unname), x::KeyedArray)
+    project_x = ProjectTo(x.data)
+    pb(y) = _KeyedArray_pullback(project_x(y), axiskeys(x))
+    return keyless_unname(x), pb
+end
+
+function ChainRulesCore.rrule(::typeof(keyless_unname), x)
+    pb(y) = (NoTangent(), y)
     return keyless_unname(x), pb
 end
--- test/_chainrules.jl
+++ test/_chainrules.jl
@@ -1,10 +1,20 @@
 @testset "chainrules.jl" begin
-    function FiniteDifferences.to_vec(k::KeyedArray)
+
+    function FiniteDifferences.to_vec(k::AxisKeys.KaNda)
         v, b = to_vec(k.data)
-        back(x) = KeyedArray(b(x); AxisKeys.named_axiskeys(k)...)
+        back(x) = wrapdims(b(x); AxisKeys.named_axiskeys(k)...)
         return v, back
     end
+
+    function FiniteDifferences.to_vec(k::KeyedArray)
+        v, b = to_vec(k.data)
+        back(x) = wrapdims(b(x), axiskeys(k)...)
+        return v, back
+    end
+
     test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), a=1:3); check_inferred=false)
     test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3, 4), a=1:3, b='a':'d'); check_inferred=false)
     test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), a=1:3); output_tangent=rand(3, 1), check_inferred=false)
+    test_rrule(AxisKeys.keyless_unname, KeyedArray(ones(3), 1:3); check_inferred=false)
+    test_rrule(AxisKeys.keyless_unname, ones(3); check_inferred=false)
 end
